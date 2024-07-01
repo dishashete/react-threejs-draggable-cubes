404bfbd57651d8eb3866dfde4d5daae8792420e6
Lalit-Shringare <146646711+Lalit-Shringare@users.noreply.github.com>
2024-07-01 12:55:28 +0530

    Update Cube.js

--- Cube.js
+++ Cube.js
@@ -1,25 +1,25 @@
 import React from "react";
 import { useDrag } from "react-dnd";
-import { colors } from "./CubeMesh"; // Assuming colors are defined in CubeMesh
+import { colors } from "./CubeMesh";
 
 const Cube = ({ id, name }) => {
-  const [{isDragging}, drag] = useDrag(() => ({
+  const [{ isDragging }, drag] = useDrag(() => ({
     type: "CUBE",
-    item: { id, name }, // Include name in the item being dragged
+    item: { id, name },
     collect: (monitor) => ({
       isDragging: !!monitor.isDragging(),
     }),
   }));
 
-  const colorIndex = id % colors.length;
+  const colorIndex = id % colors.length; 
   const color = colors[colorIndex];
 
   return (
     <div
       ref={drag}
       style={{
-        width: "100px",
-        height: "100px",
+        width: "65px",
+        height: "50px",
         backgroundColor: color,
         color: "white",
         display: "flex",
@@ -28,9 +28,13 @@ const Cube = ({ id, name }) => {
         cursor: "move",
         boxShadow: "0 0 10px rgba(0, 0, 0, 0.2)",
         opacity: isDragging ? 0.5 : 1,
+        textAlign: "center",
+        fontSize: "12px",
+        padding: "5px",
+        borderRadius: "5px"
       }}
     >
-      {name} {/* Display name instead of just "Cube {id}" */}
+      {name}
     </div>
   );
 };
